64f2711d0fed8509a36ae6a0f728d0939bc63ae3
Bequita <hernan.beca@gmail.com>
2019-11-28 11:12:28 -0300

    Code - First attempt to obtain scheluded spring config var.

--- src/main/java/backend/ViendasYaApplication.java
+++ src/main/java/backend/ViendasYaApplication.java
@@ -2,10 +2,12 @@ package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties
 public class ViendasYaApplication {
 	public static void main(String[] args) {
         SpringApplication.run(ViendasYaApplication.class,args);
--- src/main/java/backend/controller/configuration/ScheduledJobs.java
+++ src/main/java/backend/controller/configuration/ScheduledJobs.java
@@ -1,7 +1,9 @@
 package backend.controller.configuration;
 
+import backend.service.CommunicationService;
 import backend.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +14,8 @@ public class ScheduledJobs {
 
     @Autowired
     JobService jobService;
+    @Autowired
+    CommunicationService communicationService;
 
     @Scheduled(cron = "0 0 23 ? * ?", zone = "UTC")
     public void sendCustomersPurchasesEmail() throws MessagingException {
@@ -21,5 +25,10 @@ public class ScheduledJobs {
     @Scheduled(cron = "0 15 23 ? * ?", zone = "UTC")
     public void sendSuppliersSalesEmail() throws MessagingException {
         jobService.sendSuppliersSalesEmail();
+    }
+
+    @Scheduled(cron = "${spring.scheduler.cronjob}", zone = "UTC")
+    public void sendFakeEmail() throws MessagingException {
+        communicationService.sendInvalidServiceEmail("hernan.beca@gmail.com", "Oh man, your service sucks!", "TESTING");;
     }
 }

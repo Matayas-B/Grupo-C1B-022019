5b5a74d533aa210330690457ee5febebd2309944
Bequita <hernan.beca@gmail.com>
2019-11-28 11:47:07 -0300

    Code - Added global cronjobtime property.

--- src/main/java/backend/controller/configuration/ScheduledJobs.java
+++ src/main/java/backend/controller/configuration/ScheduledJobs.java
@@ -1,9 +1,7 @@
 package backend.controller.configuration;
 
-import backend.service.CommunicationService;
 import backend.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,21 +12,14 @@ public class ScheduledJobs {
 
     @Autowired
     JobService jobService;
-    @Autowired
-    CommunicationService communicationService;
 
-    @Scheduled(cron = "0 0 23 ? * ?", zone = "UTC")
+    @Scheduled(cron = "${spring.scheduler.cronjobtime}", zone = "UTC")
     public void sendCustomersPurchasesEmail() throws MessagingException {
          jobService.sendCustomersPurchasesEmail();
     }
 
-    @Scheduled(cron = "0 15 23 ? * ?", zone = "UTC")
+    @Scheduled(cron = "${spring.scheduler.cronjobtime}", zone = "UTC")
     public void sendSuppliersSalesEmail() throws MessagingException {
         jobService.sendSuppliersSalesEmail();
-    }
-
-    @Scheduled(cron = "${spring.scheduler.cronjob}", zone = "UTC")
-    public void sendFakeEmail() throws MessagingException {
-        communicationService.sendInvalidServiceEmail("hernan.beca@gmail.com", "Oh man, your service sucks!", "TESTING");;
     }
 }
